1ac1b069132c5cd5c3ab5a3cab85a25738d9d4ed
Atul Dwivedi <er.dwivediatul@gmail.com>
2018-06-17 11:02:57 +0530

    commented annotation used at method level

--- src/com/jm2007/annotation/MyClassWithAnnotation.java
+++ src/com/jm2007/annotation/MyClassWithAnnotation.java
@@ -8,10 +8,10 @@ learners = { "NagMohan", "Akshay", "Ajay", "Bikash" },
 version = 1.0)
 public class MyClassWithAnnotation {
 
-	@ClassInfo(author = "Atul Dwivedi", 
+	/*@ClassInfo(author = "Atul Dwivedi", 
 			createdDate = "2018-06-17", 
 			learners = { "NagMohan", "Akshay", "Ajay", "Bikash" }, 
-			version = 1.0)
+			version = 1.0)*/
 	public static void main(String[] args) {
 		System.out.println("This is a class with ClassInfo annotation.");
 	}
